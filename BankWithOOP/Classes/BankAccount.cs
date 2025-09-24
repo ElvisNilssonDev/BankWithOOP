750d461848e717fdb64b81711caf0d8e8be7c658
Elvis Nilsson <nilssonelvis@gmail.com>
2025-09-24 13:34:15 +0200

    I have successfully made Encapsulation making balance private in my BankAccount class.

--- BankWithOOP/Classes/BankAccount.cs
+++ BankWithOOP/Classes/BankAccount.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace BankWithOOP.Classes
 {
-    internal class BankAccount
+    public class BankAccount // This is the class definition for a BankAccount. And underneathe we have the blueprint for encapsulation.
     {
+        private decimal balance; // This field holds the account balance and its private so it can not be touched from the outside, only inside the code of this class.
+
+        public BankAccount(decimal initialBalance) // Constructor to initialize the account with an initial balance (decimal is a more accurate way of doing float of double).
+        {
+            balance = initialBalance;
+        }
+
+        public void Deposit(decimal amount) // Method to deposit money into the account.
+        {
+            if (amount > 0)
+                balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= balance) // Check if there are sufficient funds before withdrawing. (I learnt that && means AND in a more code kinda way).
+                balance -= amount;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
     }
 }
