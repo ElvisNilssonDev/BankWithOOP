570b43eda9273c1b992ab02de678cd3c87760a3e
Elvis Nilsson <nilssonelvis@gmail.com>
2025-09-24 13:20:33 +0200

    Add project files.

---
+++ BankWithOOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankWithOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
